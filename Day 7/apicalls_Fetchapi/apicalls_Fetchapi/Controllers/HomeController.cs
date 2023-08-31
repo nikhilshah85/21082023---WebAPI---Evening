e179608f11ac67fa8979e02328fed2493b170d90
Nikhil <nikhilshah.net@hotmail.com>
2023-08-31 18:29:49 +0530

    API calls - Fetch

---
+++ Day 7/apicalls_Fetchapi/apicalls_Fetchapi/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using apicalls_Fetchapi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace apicalls_Fetchapi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+        public IActionResult PostData()
+        {
+            return View();
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
